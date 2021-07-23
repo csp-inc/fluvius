b9adc126a962ccd9492b2da4dcc580d727bc363b
Vincent Landau <vincent@csp-inc.org>
2021-07-23 16:34:59 -0600

    clean up docker stuff, add run script without launching jupyter

--- run_python.sh
+++ run_python.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-if [ -z "$1" ]
-then 
-	PORT=8888
-else
-	PORT=$1
-fi
-
 TAG=data
 IMAGE=cspinc/fluvius
 IMAGENAME=$IMAGE:$TAG
@@ -15,13 +8,10 @@ if [[ "$(docker images -q $IMAGE 2> /dev/null)" == "" ]]; then
   echo "Image not found, building from recipe...."
   docker build --rm -t $IMAGENAME - < $DPATH
 fi
- 
-IP=0.0.0.0
+
 BLOB=$BLOBCONTAINER
 docker run --rm \
-	-v $('pwd'):/content \
-	-w /content \
+	-v $('pwd'):/fluvius \
+	-w /fluvius \
 	-v $BLOB:/blob \
-	-p $PORT:$PORT \
-	$IMAGENAME \
-	jupyter notebook --port $PORT --ip $IP --no-browser --allow-root
+	$IMAGENAME 

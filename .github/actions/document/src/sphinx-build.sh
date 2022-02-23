a86b87f0f0a6b935479bd6d14899fd7dd83de856
Luke Zachmann <lzachmann@gmail.com>
2022-02-23 11:07:43 -0700

    reverse steps

--- .github/actions/document/src/sphinx-build.sh
+++ .github/actions/document/src/sphinx-build.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
-# debugging
-echo "Evaluating: pwd"
-pwd
-echo "Evaluating: ls -a /src"
-ls -a /src
-echo "Evaluating: ls -a content"
-ls -a content
-echo "Evaluating: ls -a"
-ls -a
-echo "Evaluating: ls -a content/src"
-ls -a content/src
+# # debugging
+# echo "Evaluating: pwd"
+# pwd
+# echo "Evaluating: ls -a /src"
+# ls -a /src
+# echo "Evaluating: ls -a content"
+# ls -a content
+# echo "Evaluating: ls -a"
+# ls -a
+# echo "Evaluating: ls -a content/src"
+# ls -a content/src
 
+WD=$(pwd)
 mv content /content
 cp /content/example_credentials /content/credentials # use dummy credentials to build docs
 cd /content/docs
 make html
+
+echo "Evaluating: ls -a build/html"
+ls -a build/html
+
+cd $WD
+mv /content content
 
 EXIT_STATUS=$?
 echo "::set-output name=code::$EXIT_STATUS"

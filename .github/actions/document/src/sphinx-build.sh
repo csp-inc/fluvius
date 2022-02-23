81ed40132f904a6f1035fcc1665069f730affea8
Luke Zachmann <lzachmann@gmail.com>
2022-02-23 10:44:08 -0700

    debugging build context

--- .github/actions/document/src/sphinx-build.sh
+++ .github/actions/document/src/sphinx-build.sh
@@ -4,14 +4,14 @@ echo "pwd"
 pwd
 echo "ls -a /src"
 ls -a /src
-echo "ls -a /content"
-ls -a /content
+echo "ls -a content"
+ls -a content
 echo "ls -a"
 ls -a
-cd /content/docs
-echo "ls -a /content/src"
-ls -a /content/src
-make html
+# cd content/docs
+# echo "ls -a content/src"
+# ls -a content/src
+# make html
 
 EXIT_STATUS=$?
 echo "::set-output name=code::$EXIT_STATUS"
